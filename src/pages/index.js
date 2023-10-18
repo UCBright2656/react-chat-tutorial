2a9ac244b36b75e1894c7caec9e3f7dd42835816
UCBright2656 <146842627+UCBright2656@users.noreply.github.com>
2023-10-18 17:59:57 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,7 +135,7 @@ export default function Home() {
             onSendMessage={onSendMessage}
             onChangeTypingState={onChangeTypingState}
           />
-          <a className={styles.upsell} href='https://www.scaledrone.com/blog/tutorial-build-a-reactjs-chat-app/'>Real-time React chat using Scaledrone. See full tutorial →</a>
+         // <a className={styles.upsell} href='https://www.scaledrone.com/blog/tutorial-build-a-reactjs-chat-app/'>Real-time React chat using Scaledrone. See full tutorial →</a>
         </div>
       </main>
     </>
